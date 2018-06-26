b722889348db1cfd53d6f70cbe4b836531ac4b3a
Kay Ogunbowale <o.ogunbowal@gmail.com>
2018-06-26 12:04:59 +0100

    Intial!

--- app/src/main/java/com/enigmasoftltd/android/prod/journalapp/MainActivity.java
+++ app/src/main/java/com/enigmasoftltd/android/prod/journalapp/MainActivity.java
@@ -1,19 +1,15 @@
 package com.enigmasoftltd.android.prod.journalapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mText = findViewById(R.id.testTV);
-        mText.setText("Kay");
     }
 }
